aa8661ae281bba43e8f1b52724db57ba2a144eb6
TerminalVibes <87617218+TerminalVibes@users.noreply.github.com>
2024-08-21 23:40:04 -0400

    Removed sweeping declarations to another package

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,5 @@
 /// <reference no-default-lib="true"/>
 /// <reference types="@rbxts/types"/>
-/// <reference path="roblox.d.ts" />
 /// <reference path="api/cache.d.ts" />
 /// <reference path="api/closures.d.ts" />
 /// <reference path="api/console.d.ts" />
@@ -14,21 +13,3 @@
 /// <reference path="api/misc.d.ts" />
 /// <reference path="api/scripts.d.ts" />
 /// <reference path="api/websocket.d.ts" />
-
-// Override DataModel Methods
-interface DataModel {
-	HttpGet(url: string): string;
-}
-
-// Override Services
-interface Services {
-	CoreGui: CoreGui;
-	VirtualInputManager: VirtualInputManager;
-	VirtualUser: VirtualUser;
-}
-
-declare module "@rbxts/services" {
-	export const CoreGui: Services["CoreGui"];
-	export const VirtualInputManager: Services["VirtualInputManager"];
-	export const VirtualUser: Services["VirtualUser"];
-}
